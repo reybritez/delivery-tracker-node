711b920062c5ba8d36442572f9d7e84ce7548095
Steven Rey <stevenreybritez@gmail.com>
2022-06-16 12:55:51 -0400

    Cambios en parse y api getter

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .post("/api", (req, res) => {
+    console.log(req.body)
+    res.status(200).end()})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
